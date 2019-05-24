3a9c5b81bf1b242f4a02e656c89f1126ccabe71f
Héctor Costa <hcostaguzman@gmail.com>
2019-05-24 02:56:32 +0200

    p80

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ io.on('connection', (socket) => {
   });
 })
 
-var server = http.listen(5000, () => {
-  console.log("Servidor listo en http://127.0.0.1:5000");
+var server = http.listen(80, () => {
+  console.log("Servidor listo en http://127.0.0.1");
 });
 
 
